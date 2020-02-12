eca014b8659f57ae545078acbea1ca163f2565ac
山崎拓己 <yamasakitakumi1190@se-project.co.jp>
2020-02-12 10:54:08 +0900

    論理削除の実施

--- SaleManagement/src/main/java/com/example/demo/SaleService.java
+++ SaleManagement/src/main/java/com/example/demo/SaleService.java
@@ -48,4 +48,26 @@ public class SaleService {
 	public Client getOne(String no) {
 		return displayRepository.getOne(no);
 	}
+
+	public void delete(SaleRequest saleRequest) {
+
+		Client customer = new Client();
+		customer.setNo(saleRequest.getNo());
+		customer.setClient(saleRequest.getClient());
+		customer.setOrder_date(saleRequest.getOrder_date().replace("/",""));
+		customer.setS_number(saleRequest.getS_number());
+		customer.setSubject(saleRequest.getSubject());
+		customer.setQuantity(saleRequest.getQuantity());
+		customer.setDelivery_date(saleRequest.getDelivery_date().replace("/",""));
+		customer.setDue_date(saleRequest.getDue_date().replace("/",""));
+		customer.setBilling_date(saleRequest.getBilling_date().replace("/",""));
+		customer.setEstimated_amount(saleRequest.getEstimated_amount());
+		customer.setOrder_amount(saleRequest.getOrder_amount());
+		customer.setStatus_number(saleRequest.getStatus_number());
+		customer.setRemarks(saleRequest.getRemarks());
+		customer.setId("1");
+		customer.setDelete_flg("1");
+
+		displayRepository.save(customer);
+	}
 }
